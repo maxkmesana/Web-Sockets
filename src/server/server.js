6984d4a58d79820e41b8a8cb49ca6deddfa6965c
Max <max.kmesana@gmail.com>
2024-11-25 22:57:44 -0300

    broken :(

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
 
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -11,7 +12,7 @@ const io = new Server(server, {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '../../index.html');
   });
 
 io.on("connection", (socket) => {
